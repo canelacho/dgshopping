bb461190a73b47c55050e0ac5ec1b9813ccdbdeb
Hernan Piña <hdpldaniel@gmail.com>
2017-04-19 16:42:33 -0500

    test upload

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect('mongodb://localhost/dgshopping', function(err, res) {
 
 var app = express()
 
+
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
