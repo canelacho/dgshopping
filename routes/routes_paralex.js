b477861bcc2f44c7a380e63cf8f4c71576b8bb5b
Hernan Piña <hdpldaniel@gmail.com>
2017-05-21 21:30:36 -0500

    fixed paralex automatic

--- routes/routes_paralex.js
+++ routes/routes_paralex.js
@@ -6,13 +6,16 @@ module.exports = function(app) {
   		    cb(null, './public/img/paralex/')
   		  },
   		  filename: function (req, file, cb) {
-  		    cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.').pop())
+  		    // cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.').pop())
+  		    cb(null, 'paralex.jpg')
   		  }
   		}),
 			upload = multer({ storage: storage })
 
 // POST
 	var addItem = function(req, res, next) {
+		
+		console.log('aqui va el viejo')
     console.log(storage.filename)
     console.log('req.files: ' + req.files)
     var photos = []
@@ -21,9 +24,35 @@ module.exports = function(app) {
 		}
     console.log('fotos: ' + photos)
 
+
+		console.log('=============================')
+		fs.readdir('./public/img/paralex/', function(err, files){
+			if (err) {
+		    return console.error(err);
+		  }
+		  files.forEach( function (file){
+		  	console.log('=============================')
+		  	console.log( file );
+		  	console.log('=============================')
+		  	console.log(photos[0])
+
+		  		if(file !== photos){
+		  	
+			  	// 	fs.unlink('./public/img/paralex/'+file, function(err){
+						// if (err) {
+				  //   	return console.error(err);
+				  //  	}
+				  //  	console.log("File deleted successfully!");
+					//})
+		  	}
+		  });
+		})
+
     // res.send(newItem)
 		res.render('private/dashboard')
 	}
+
+
 
  // API ROUTES
   app.post('/paralex', upload.any(), addItem)
